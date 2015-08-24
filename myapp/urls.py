0522cc760886da81f1cb2cbf565aa4c1810491e7
Austin Meyers <meyersaustin22@gmail.com>
2015-08-24 01:12:40 +0000

    Fixed view issues

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.image_list, name='image_list'),
 	url(r'^id/(?P<id>[0-9]+)', views.image_page, name='image_page'),
-	url('^add', views.image_add, name='image_add'),
+	url(r'^add', views.image_add, name='image_add'),
 	url(r'^delete/(?P<id>[0-9]+)', views.image_delete, name='image_delete'),
 ]
-
