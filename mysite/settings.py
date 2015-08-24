dea62a040911f364d7fc65145689ecb9b7721cc5
Austin Meyers <meyersaustin22@gmail.com>
2015-08-24 02:07:12 +0000

    View structuring

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/relatim/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
+STATIC_ROOT = 'static/'
+STATIC_URL = 'static/'
